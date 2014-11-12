9044e4a9a46bfb6c00fa83983b55259c86309c4d
Pawel Socha <psocha@gmail.com>
2014-11-12 11:02:02 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-console.log(process.argv);
+console.log("Node starting...");
+var buffer = '';
+var fs = require('fs');
+fs.readFile('./file1.txt', function(err, data) {
+     buffer = data.toString();  // buffer object
+     console.log(buffer);
+});
+ 
+ 
+fs.watchFile('./file1.txt', {interval:10}, function(prev, curr) {
+  console.log(fs.readFile('./curr', function(err, data){
+  				x = data.toString();
+  				console.log(x);
+  			}
+  	));
+  // display an update was made, then console.log the new updated file!
+  
+});
+
+process.argv.forEach(function(el, index){
+		if(index > 1){
+			console.log(el)}
+})
+//console.log(process.argv);
 
 
 
--- sync.js
+++ sync.js
@@ -1,4 +1,13 @@
-console.log("Node starting...");
-// var fs = require('fs');
-// console.log(fs);
-// console.log(fs.readFileSync('./file1.txt'));
+//console.log("Node starting...");
+
+var nodeStartingFunctionFileCode = require("./nodeStartingFunction");
+//console.log(nodeStartingFunctionFileCode);
+nodeStartingFunctionFileCode.nodeStartingFunction();
+var fs = require('fs');
+//console.log(fs);
+//console.log(fs.readFileSync('./file1.txt'));
+var buffer = ''
+fs.readFile('./file1.txt', function(err, data) {
+     buffer = data.toString();  // buffer object
+     console.log(buffer);
+});
